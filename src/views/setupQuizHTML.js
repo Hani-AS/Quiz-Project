67f45b37cc465399a6d7247f09287a88806f1326
Ameri <mohammedagl6@gmail.com>
2021-06-03 23:43:33 +0200

    updated more styling

--- src/views/setupQuizHTML.js
+++ src/views/setupQuizHTML.js
@@ -3,6 +3,7 @@
 import createDOMElement from "../utils/createDOMElement.js";
 import createNextQuestionButtonElement from './createNextQuestionButtonElement.js'
 import {TIMER_ID, SCORE_ID, QUESTION_CONTAINER_ID, QUIZ_CONTAINER_ID} from '../constants.js';
+import { addClass } from "../utils/manageClass.js";
 
 
 
@@ -10,15 +11,26 @@ const setupQuizHTML = () =>{
   const quizContainer = createDOMElement('div', { id: QUIZ_CONTAINER_ID });
   const scoreContainer = createDOMElement('div');
   const scoreText = createDOMElement('p', {id: SCORE_ID});
-  const timerContainer = createDOMElement('p', {id: TIMER_ID});
+  scoreContainer.appendChild(scoreText);
+  addClass(scoreContainer, 'questions_info_bar-score');
+
+  const timerContainer = createDOMElement('div');
+  const timerContent = createDOMElement('p');
+  timerContent.innerHTML = `Time left: <span id="${TIMER_ID}"></span> seconds`;
+  timerContainer.appendChild(timerContent);
+  addClass(timerContainer, 'questions_info_bar-timer');
+
+  const topBar = createDOMElement('div');
+  addClass(topBar, 'questions_info_bar');
+  topBar.appendChild(scoreContainer);
+  topBar.appendChild(timerContainer);
+
   const questionContainer = createDOMElement('div', {
     id: QUESTION_CONTAINER_ID,
   });
   
-  quizContainer.appendChild(timerContainer);
-  scoreContainer.appendChild(scoreText);
-  quizContainer.appendChild(scoreContainer);
-
+  quizContainer.appendChild(topBar);
+  
   quizContainer.appendChild(questionContainer);
 
   const nextQuestionButton = createNextQuestionButtonElement();
