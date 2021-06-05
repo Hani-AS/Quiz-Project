72cc506193b3f58b4d84881230f4f9f526dfba3a
Mansour Ibrahim <engmans255@gmail.com>
2021-06-05 18:28:31 +0200

    finishing-the-result-page

--- src/constants.js
+++ src/constants.js
@@ -13,3 +13,4 @@ export const QUIZ_CONTAINER_ID = 'quiz-container';
 export const NEXT_QUESTION_BUTTON_ID = 'next-question-button';
 export const SCORE_ID = "total-score";
 export const TIMER_ID = "timer";
+export const RESULT_IMG_CONTAINER = "result-img";
--- src/views/createResetButton.js
+++ src/views/createResetButton.js
@@ -1,9 +1,11 @@
 'use strict';
 import resetTheQuiz from '../listeners/resetTheQuiz.js';
 import createDOMElement from '../utils/createDOMElement.js';
+import { addClass } from '../utils/manageClass.js';
 
 const createResetButton = () => {
   const resetButton = createDOMElement('button');
+  addClass(resetButton, 'reset-button');
   resetButton.addEventListener('click', resetTheQuiz);
   resetButton.innerText = 'Reset the quiz';
   return resetButton;
